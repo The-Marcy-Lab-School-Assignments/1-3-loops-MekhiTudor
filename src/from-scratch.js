116797c1acf25287d22b29eef0fcb98a730f59e6
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-04-07 19:14:12 -0400

    everything done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -37,7 +37,24 @@ const loopUpToNum = (num) => {
 //loopUpToNum(-10)
 
 const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz')
+      continue;
+    }
+    if (i % 3 === 0) {
+      console.log('fizz');
+      continue;
+    }
+    if (i % 5 === 0) {
+      console.log('buzz')
+      continue;
+    }
+
+    console.log(i)
+  }
 };
+fizzbuzz()
 
 module.exports = {
   loop0UpTo10,
