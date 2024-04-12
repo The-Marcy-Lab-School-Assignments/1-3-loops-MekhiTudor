6af46945453f72c0486403768e1b7e15fb31760e
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-04-11 23:24:17 -0400

    feedback

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,3 +1,4 @@
+/** FEEDBACK: Awesome job!  */
 const loop0UpTo10 = () => {
   for (let i = 0; i < 10; i++) {
     console.log(i);
@@ -35,7 +36,24 @@ const loopUpToNum = (num) => {
   }
 };
 //loopUpToNum(-10)
-
+/** FEEDBACK: I love that you are covering your bases through the use of the keyword continue, however I do want to name that if you were to use if/else statements, you could avoid being specific with many continue keywords as the if/else statements would be interconnected and based off of one another. 
+ * The code could look like this instead:
+ * const fizzbuzz = () => {
+    for (let i = 1; i <= 100; i++) {
+      if (i % 3 === 0 && i % 5 === 0){
+        console.log('fizzbuzz')
+      } else if (i % 5 === 0){
+        console.log('buzz')
+      } else if (i % 3 === 0){
+        console.log('fizz')
+      } else {
+        console.log(i)
+      }
+    };
+  };
+ * Notice the conditions and how they go from very specific to broad! 
+ * Also notice the way I spaced things out so that I could see where one condition ended and another started! 
+ */
 const fizzbuzz = () => {
   for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
